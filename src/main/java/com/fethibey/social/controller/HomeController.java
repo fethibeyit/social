f1f31856751749ca0a8db35eb52dd451325a00ed
fethi bey abi ayad <fethibeyabiayad@gmail.com>
2023-07-18 21:41:14 -0400

    Add Home controller

---
+++ src/main/java/com/fethibey/social/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.fethibey.social.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/home")
+public class HomeController {
+
+    @GetMapping
+    public String Home(){
+        return "hello world";
+    }
+}
