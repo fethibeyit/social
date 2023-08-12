e98fd87199a083a98fb9b4d7197692aa7f158da1
fethi bey abi ayad <fethibeyabiayad@gmail.com>
2023-08-11 23:14:09 -0400

    add ngrx effect to project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,6 +11,7 @@ import {SharedModule} from "./shared/shared.module";
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import {environment} from "../environments/environment";
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import {environment} from "../environments/environment";
     MatCardModule,
     SharedModule,
     StoreModule.forRoot({}, {}),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production})
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production}),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
