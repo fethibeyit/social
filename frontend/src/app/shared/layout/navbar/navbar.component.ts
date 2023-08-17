9fe4d41513df7aad21830eccd9710c2869c43524
fethi bey abi ayad <fethibeyabiayad@gmail.com>
2023-08-16 22:59:56 -0400

    Add ogout navbar

--- frontend/src/app/shared/layout/navbar/navbar.component.ts
+++ frontend/src/app/shared/layout/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  @Output() actionEmitter = new EventEmitter();
+  @Input() loggedIn = false;
+  submit(action: string) {
+    this.actionEmitter.emit(action);
+  }
 
 }
