1e20428e3b98172b7df5b60923ae70259d5d188e
fethi bey abi ayad <fethibeyabiayad@gmail.com>
2023-09-02 19:55:06 -0400

    add system.properties

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: "https://social-angular-app-e449d9352649.herokuapp.com/api/v1",
+  authURL: "https://social-angular-app-e449d9352649.herokuapp.com"
 };
