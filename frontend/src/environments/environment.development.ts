bd186fdef2161a8719b57c73180cf86449c3ea43
fethi bey abi ayad <fethibeyabiayad@gmail.com>
2023-09-17 21:43:33 -0400

    Add Register endpoint and fix prod url

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
+  domainURL : "localhost:8080",
   apiURL: "http://localhost:8080/api/v1",
   authURL: "http://localhost:8080"
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  domainURL : "social-app-z771.onrender.com",
   apiURL: "https://social-app-z771.onrender.com/api/v1",
   authURL: "https://social-app-z771.onrender.com"
 };
