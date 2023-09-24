0a81ca28620995218421303b07742ed4b5bcf4d2
fethi bey abi ayad <fethibeyabiayad@gmail.com>
2023-09-23 20:44:30 -0400

    Add socialProvider

---
+++ src/main/java/com/fethibey/social/enums/SocialProvider.java
@@ -0,0 +1,22 @@
+package com.fethibey.social.enums;
+
+public enum SocialProvider {
+
+    FACEBOOK("facebook"),
+    TWITTER("twitter"),
+    LINKEDIN("linkedin"),
+    GOOGLE("google"),
+    GITHUB("github"),
+    LOCAL("local");
+
+    private String providerType;
+
+    public String getProviderType() {
+        return providerType;
+    }
+
+    SocialProvider(final String providerType) {
+        this.providerType = providerType;
+    }
+
+}
