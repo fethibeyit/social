ba9467b61d219c04c232b436215104e8e6fc5793
fethi bey abi ayad <fethibeyabiayad@gmail.com>
2024-01-13 15:01:54 -0500

    remove Material components from File Module

--- frontend/src/app/file/file.module.ts
+++ frontend/src/app/file/file.module.ts
@@ -2,21 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DragNDropComponent} from "./components/drag-n-drop/drag-n-drop.component";
 import {DragNDropDirective} from "./directives/drag-n-drop.directive";
-import {DialogConfirmComponent} from "./components/dialog-confirm/dialog-confirm.component";
 import {NgxsModule} from "@ngxs/store";
 import {FileState} from "./state/file-state";
 import {CdkVirtualScrollViewport} from "@angular/cdk/scrolling";
 import {TranslateModule} from "@ngx-translate/core";
-import {MatDialogModule} from "@angular/material/dialog";
-import {MatButtonModule} from "@angular/material/button";
-import {MatCardModule} from "@angular/material/card";
-import {MatIconModule} from "@angular/material/icon";
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
 
 @NgModule({
   declarations: [
     DragNDropComponent,
-    DragNDropDirective,
-    DialogConfirmComponent
+    DragNDropDirective
   ],
   exports: [
     DragNDropComponent
@@ -26,10 +22,8 @@ import {MatIconModule} from "@angular/material/icon";
     NgxsModule.forFeature([FileState]),
     CdkVirtualScrollViewport,
     TranslateModule,
-    MatDialogModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule
+    CardModule,
+    ButtonModule
   ]
 })
 export class FileModule { }
